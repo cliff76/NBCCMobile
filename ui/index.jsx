9cf5ce853c745d8bb5f7c9b78e08dd6401b9973c
Clifton Craig <c.craig@samsung.com>
2018-08-29 15:22:52 -0700

    Adds initial member checkin list.

--- ui/index.jsx
+++ ui/index.jsx
@@ -2,9 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { FaPencilAlt } from 'react-icons/fa';
+
 class App extends React.Component {
     render() {
-        return <h1>Hello world</h1>;
+        return (
+            <div className={'container card-header'}>
+                <div className={'row'}>
+                    <div className={'col'}>Check In</div>
+                </div>
+                <div className={'row'}>
+                    <div className={'col'}>Mary Gunderson</div>
+                    <div className={'col'}><button className={'btn-primary'}><FaPencilAlt/></button></div>
+                </div>
+                <div className={'row'}>
+                    <div className={'col'}>Lucas Rose</div>
+                    <div className={'col'}><button className={'btn-primary'}><FaPencilAlt/></button></div>
+                </div>
+                <div className={'row'}>
+                    <div className={'col'}>George Fouché</div>
+                    <div className={'col'}><button className={'btn-primary'}><FaPencilAlt/></button></div>
+                </div>
+            </div>
+        );
     }
 }
 
