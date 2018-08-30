24351430f35022804a96f1e37dd215d1a58eea53
Clifton Craig <c.craig@samsung.com>
2018-08-29 20:59:22 -0700

    Adds editing capability.

--- ui/index.jsx
+++ ui/index.jsx
@@ -2,30 +2,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { FaPencilAlt } from 'react-icons/fa';
+import { FaPencilAlt, FaSave } from 'react-icons/fa';
 
 class Member extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             name: props.name,
-            isCheckedIn: false
+            isCheckedIn: false,
+            isEditing: false
         };
+        this.handleEditClick = this.handleEditClick.bind(this);
+        this.handleSaveClick = this.handleSaveClick.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleCheckinClick = this.handleCheckinClick.bind(this);
     }
-    render() {
+
+    handleEditClick() {
+        this.setState(Object.assign(this.state, {isEditing:true}))
+    }
+
+    handleSaveClick() {
+        this.setState(Object.assign(this.state, {isEditing:false}))
+    }
+
+    handleNameChange(e) {
+        this.setState(Object.assign(this.state, {name:e.target.value}))
+    }
+
+    handleCheckinClick(e) {
+        this.setState(Object.assign(this.state, {isCheckedIn:e.target.checked}))
+    }
+
+    renderEdit() {
+        console.log('state', this.state);
         return (
             <div className={'row'}>
-                <div className={'col'}>{this.state.name}</div>
-                <div className={'col'}><button className={'btn-primary'}><FaPencilAlt/></button></div>
+                <div className="col"><input value={this.state.name} onChange={this.handleNameChange}/></div>
+                <div className="col">Checked in? <input type="checkbox" onChange={this.handleCheckinClick} checked={this.state.isCheckedIn}/></div>
+                <div className="col"><button className="btn-primary" onClick={this.handleSaveClick}><FaSave/></button></div>
+            </div>
+        )
+    }
+
+    renderDisplay() {
+        return (
+            <div className={'row'}>
+                <div className="col">{this.state.name}</div>
+                <div className="col">
+                    <button className="btn-primary" onClick={this.handleEditClick}>
+                        <FaPencilAlt/>
+                    </button>
+                </div>
             </div>
         );
+    }
+
+    render() {
+        return this.state.isEditing ? this.renderEdit() : this.renderDisplay();
     }
 }
 
 class App extends React.Component {
     render() {
         return (
-            <div className={'container card-header'}>
+            <div className="container card-header">
                 <div className={'row'}>
                     <div className={'col'}>Check In</div>
                 </div>
