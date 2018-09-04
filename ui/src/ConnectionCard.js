53bb41a2dc07a178e04b993e2c54b1c0ebbf5dc6
Clifton Craig <clifton.craig@gmail.com>
2018-09-03 19:56:46 -0700

    Binds form fields to ConnectionCard state.

--- ui/src/ConnectionCard.js
+++ ui/src/ConnectionCard.js
@@ -2,19 +2,38 @@ import React, {Component} from "react";
 import {Radio, Text} from "./form-fields";
 
 class ConnectionCard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+
+  handleOnChange(e) {
+    const update = {[e.target.name]: e.target.value};
+    this.setState(prevState => {
+      return {...prevState, ...update};
+    });
+  }
+
   render() {
     return (
-      <div className="App-intro">
+      <form className="App-intro">
         Contact Information
-        <br/> <Text placeholder="First Name" required/> <Text placeholder="Last Name" required/>
-        <br/> <Text placeholder="Email"/>
-        <br/> <Text placeholder="Mobile Phone"/> <Text placeholder="Home Phone"/>
-        <br/> <Text placeholder="Street"/>
-        <br/> <Text placeholder="City"/> <Text placeholder="State"/> <Text placeholder="Zip"/>
-        <br/> <Text placeholder="Birthday"/> <Text placeholder="Gender"/>
-        <br/> <Text placeholder="Marital Status"/>
-        <br/> <Radio name="gathering" label="Gathering Attended" options="8:30 AM, 10:15 AM, 12 Noon" required/>
-      </div>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="firstname" placeholder="First Name" required/>
+              <Text onChange={this.handleOnChange.bind(this)} name="lastname" placeholder="Last Name" required/>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="email" placeholder="Email"/>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="mobilephone" placeholder="Mobile Phone"/>
+              <Text onChange={this.handleOnChange.bind(this)} name="homephone" placeholder="Home Phone"/>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="street" placeholder="Street"/>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="city" placeholder="City"/>
+              <Text onChange={this.handleOnChange.bind(this)} name="state" placeholder="State"/>
+              <Text onChange={this.handleOnChange.bind(this)} name="zip" placeholder="Zip"/>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="birthday" placeholder="Birthday"/>
+              <Text onChange={this.handleOnChange.bind(this)} name="gender" placeholder="Gender"/>
+        <br/> <Text onChange={this.handleOnChange.bind(this)} name="maritalstatus" placeholder="Marital Status"/>
+        <br/> <Radio onChange={this.handleOnChange.bind(this)} name="gathering" label="Gathering Attended" options="8:30 AM, 10:15 AM, 12 Noon" values="[8:30], [10:15], [12:00]" required/>
+        <br/> <Radio onChange={this.handleOnChange.bind(this)} name="status" label="I am a:" options="1st time guest, 2nd time guest, Regular attendee" values="1st, 2nd, Reg" required/>
+      </form>
     );
   }
 }
--- ui/src/form-fields.js
+++ ui/src/form-fields.js
@@ -10,23 +10,26 @@ function Text(props) {
 
   const placeholder = props.placeholder + (props.required ? '*' : '');
   return (
-    <Generic type="text" placeholder={placeholder}/>
+    <Generic type="text" name={props.name} onChange={props.onChange} placeholder={placeholder}/>
   );
 }
 
 function RadioButton(props) {
   return (
     <span key={props.key}>
-      <Generic type="radio" name={props.name}/> {props.text}
+      <Generic type="radio" onChange={props.onChange} name={props.name} value={props.value}/> {props.text}
     </span>
   );
 }
 
 function Radio(props) {
+  const fields = props.options.split(',');
+  const values = props.values.split(',');
+  if(fields.length > values.length) throw 'Invalid Number of values for "' + props.name + '". you specified ' + fields.length + ' options and ' + values.length + ' values!';
   return (
     <fieldset>
       <legend>{props.label + (props.required ? '*' : '')}</legend>
-      {props.options.split(',').map((each, i) => RadioButton({key:i, name:props.name, text:each}))}
+      {fields.map((each, i) => RadioButton({key:i, onChange:props.onChange, value:values[i], name:props.name, text:each}))}
     </fieldset>
   );
 }
